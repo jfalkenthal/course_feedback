a37b76afabca7a5704eb9658eebbc882886083a0
libra guest <j.falkenthal@gmail.com>
2012-11-03 14:11:32 -0400

    Rails generated app with

---
+++ course_feedback/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseFeedback::Application.config.secret_token = '3495a430a4a2b8c0a6ec4e8fa55041c2ec1d622a91d031f8e02076b34adaf606a42ade5f76221fda7f719904660bef1f090b07f7932dd10d518dce93351e232a'
